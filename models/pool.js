f58de405ede33cb14be23d4cd2380951ac163eae
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-27 11:27:24 +0500

    Added the pool and the login form

---
+++ models/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = requrie("pg");
+
+module.exports = new Pool({
+  host: "localhost",
+  user: `${process.env.PSQL_USER}`,
+  database: "members",
+  password: `${encodeURIComponent(process.env.PSQL_PASS)}`,
+  port: 5432,
+});
