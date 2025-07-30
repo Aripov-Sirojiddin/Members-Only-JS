8cc9e9a540bd4617ae8f25e2cd0d635581370bfc
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-30 13:13:10 +0500

    Updated the header styles.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const { getAllMessages } = require("../models/messagesModel.js");
 
 async function getMessages(req, res) {
   const status = res.locals.currentUser ? await isMember(res.locals.currentUser.id) : false;
-
+  
   const messages = await getAllMessages(status);
   res.render("pages/index", { isMember: status, messages: messages });
 }
